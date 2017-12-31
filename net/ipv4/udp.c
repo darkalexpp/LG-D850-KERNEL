596f79a9b30bc379a5cd75bfd335061fe69b8380
darkalexpp <34772359+darkalexpp@users.noreply.github.com>
2017-12-31 11:23:56 -0500

    Update udp.c

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -1252,11 +1252,9 @@ csum_copy_err:
 	if (!skb_kill_datagram(sk, skb, flags))
 		UDP_INC_STATS_USER(sock_net(sk), UDP_MIB_INERRORS, is_udplite);
 	unlock_sock_fast(sk, slow);
-
-	if (noblock)
-		return -EAGAIN;
-
-	/* starting over for a new packet */
+	
+/* starting over for a new packet, but check if we need to yield */
+ 	cond_resched();
 	msg->msg_flags &= ~MSG_TRUNC;
 	goto try_again;
 }
